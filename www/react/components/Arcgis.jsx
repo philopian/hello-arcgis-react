b8166fc2f2df179813182e3d05948092fd39e42e
philopian <pdotwdot@gmail.com>
2017-08-30 21:29:25 -0700

    WIP trying to get a onclick handle on the Graphics Layer

--- www/react/components/Arcgis.jsx
+++ www/react/components/Arcgis.jsx
@@ -23,6 +23,33 @@ const BasemapGallery = Widgets.BasemapGallery;
 const Locate = Widgets.Locate;
 
 
+
+
+const pdx = (
+    <Graphic onClick={()=> console.log('.....????')}>
+        <Symbols.SimpleMarkerSymbol
+            onClick={()=> console.log('.....????')}
+            symbolProperties={{
+                color: [226, 119, 40],
+                outline: {
+                    color: [255, 255, 255],
+                    width: 2
+                }
+            }}
+        />
+        <Geometry.Point
+            onClick={()=> console.log('.....????')}
+            geometryProperties={{
+                latitude: 45.5231,
+                longitude: -122.6765
+            }}
+        />
+    </Graphic>
+);
+
+
+
+
 // React Component
 export default class Sample extends React.Component {
   constructor(props, context) {
@@ -33,19 +60,20 @@ export default class Sample extends React.Component {
     this.state = {
         viewProperties: {
           center: [-122.4443, 45.2529],
-        }
+        },
+        showPopup: false,
     };
 
     // Its more performate to bind your methods in the constructor
     this.mapClick = this.mapClick.bind(this);
+    this.featureLayerClick = this.featureLayerClick.bind(this);
   }
 
   mapClick() {
     console.log('... you just clicked the map!');
-    // store.dispatch({
-    //   type: 'UPDATE_SOMETHING',
-    //   payload: Date(),
-    // });
+  }
+  featureLayerClick(){
+    console.log('....feature layer click');
   }
 
   render() {
@@ -63,6 +91,37 @@ export default class Sample extends React.Component {
             }}
             onClick={this.mapClick}
         >
+            <Layers.GraphicsLayer onClick={this.featureLayerClick}>
+                {pdx}
+            </Layers.GraphicsLayer>
+
+            <Layers.FeatureLayer
+                layerProperties={{
+                    url: 'https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/OpenBeerDatabase/FeatureServer/0'
+                }}
+                popupTemplate={{
+                  title: "{name}",
+                  content: [{
+                    type: "fields",
+                    fieldInfos: [{
+                      fieldName: "name"
+                    }, {
+                      fieldName: "address1",
+                      label: "address"
+                    }, {
+                      fieldName: "city"
+                    }, {
+                      fieldName: "state"
+                    }, {
+                      fieldName: "phone"
+                    }, {
+                      fieldName: "website"
+                    }]
+                  }]
+                }}
+                onClick={this.featureLayerClick}
+            />
+
             <SearchWidget position="top-right" />
             <Locate position="top-left" />
         </MapView>
